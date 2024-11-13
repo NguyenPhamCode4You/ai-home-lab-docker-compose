563eba7751d030474bec0412e4ac68cdcf16d97f
Nicheal <nichealpham@gmail.com>
2024-11-13 21:20:13 +0100

    ok

--- scripts/agentic.py
+++ scripts/agentic.py
@@ -172,7 +172,7 @@ class SupabaseVectorStore:
         return True
 
 directory_path = './documents'
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=120, chunk_overlap=10)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=10)
 
 file_index = 0
 sentence_index = 0
