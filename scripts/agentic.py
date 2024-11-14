e224ebb50d8dc67f6c4344a731bd08512e3b96ba
Nguyen Pham <nguyen.pham@code4you.com>
2024-11-14 11:43:54 +0100

    ok

--- scripts/agentic.py
+++ scripts/agentic.py
@@ -375,7 +375,8 @@ class SupabaseVectorStore:
 
 directory_path = './documents'
 # text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=10)
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=10)
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=10)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=10)
 
 file_index = 0
 sentence_index = 0
