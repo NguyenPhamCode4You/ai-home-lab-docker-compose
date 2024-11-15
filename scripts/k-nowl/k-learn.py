fb1567a2aa994055a9d3e8c37dd607523e7f67a5
Nicheal <nichealpham@gmail.com>
2024-11-15 20:51:40 +0100

    asasdad

--- scripts/k-nowl/k-learn.py
+++ scripts/k-nowl/k-learn.py
@@ -52,7 +52,7 @@ for root, _, files in os.walk(f"./{document_path}"):
                     print(f"Content: {content}\n")
 
                     supabase.insert_embedding(text=content, embedding=embedding1, metadata=metadata, embedding2=embedding2)
-                    print(f"File {file_index}/{len(files)} - Line {line_index} - {file_path}\noooooooooooooooooooo \n\n\n\n\n")
+                    print(f"oooooooooooooooooooo File {file_index}/{len(files)} - Line {line_index} - {file_path} oooooooooooooooooooo \n\n\n\n\n")
                     line_index += 1
 
         file_index += 1
