445b5bd694171c6a0d3421ce715f98957a60cde6
Nicheal <nichealpham@gmail.com>
2024-11-17 22:32:17 +0100

    asd

--- scripts/k-nowl/h-split.py
+++ scripts/k-nowl/h-split.py
@@ -11,7 +11,7 @@ embedder = CreateEmbedding()
 from SupabaseVectorStore import SupabaseVectorStore
 SUPABASE_URL = "http://localhost:8000"
 SUPABASE_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE"
-TABLE_NAME = "n8n_documents_net_micro"
+TABLE_NAME = "n8n_documents_bbc_bvms"
 supabase = SupabaseVectorStore(SUPABASE_URL, SUPABASE_TOKEN, TABLE_NAME)
 
 section_max_length = 110
