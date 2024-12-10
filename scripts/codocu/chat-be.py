848339d33b63e7f0bb82726974c963fbb69092aa
Nguyen Pham <nguyen.pham@code4you.com>
2024-12-10 22:54:53 +0100

    Comment example of running write_analysis

--- scripts/codocu/chat-be.py
+++ scripts/codocu/chat-be.py
@@ -193,19 +193,19 @@ async def render_markdown(
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error rendering file: {e}")
     
-import asyncio
+# import asyncio
 
-question = """
-I need you to write an analysis on how BVMS calculate EU ETS. To do that, follow the below steps:
-1. Ask BVMS KnowledgeBase to explain the business logics of how BVMS calculate Bunker consumption.
-2. Ask BVMS Code Document to provide code implementation and explanation for the topic of Bunker consumption.
-3. Ask BVMS KnowledgeBase to explain the business logics of how BVMS calculate EU ETS.
-4. Ask BVMS Code Document to provide code implementation and explanation for calculate EU ETS.
-Basing on the information you gather, write an analysis on how BVMS calculate EU ETS.
-Be prefessional and detailed in your analysis, provide code snippets and documentations as needed.
-"""
-orchesrea.set_log_file(os.path.join(os.path.dirname(__file__), "task-log.md"))
-asyncio.run(orchesrea.write_analysis(question))
+# question = """
+# I need you to write an analysis on how BVMS calculate EU ETS. To do that, follow the below steps:
+# 1. Ask BVMS KnowledgeBase to explain the business logics of how BVMS calculate Bunker consumption.
+# 2. Ask BVMS Code Document to provide code implementation and explanation for the topic of Bunker consumption.
+# 3. Ask BVMS KnowledgeBase to explain the business logics of how BVMS calculate EU ETS.
+# 4. Ask BVMS Code Document to provide code implementation and explanation for calculate EU ETS.
+# Basing on the information you gather, write an analysis on how BVMS calculate EU ETS.
+# Be prefessional and detailed in your analysis, provide code snippets and documentations as needed.
+# """
+# orchesrea.set_log_file(os.path.join(os.path.dirname(__file__), "task-log.md"))
+# asyncio.run(orchesrea.write_analysis(question))
 
 
 # Run the FastAPI app
