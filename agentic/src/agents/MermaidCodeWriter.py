b509001b69e3626f35de3e677521daea2d04e467
Nicheal <nichealpham@gmail.com>
2024-12-29 20:35:09 +0100

    Update MermaidCodeWriter

--- agentic/src/agents/MermaidCodeWriter.py
+++ agentic/src/agents/MermaidCodeWriter.py
@@ -4,13 +4,10 @@ from .models.Ollama import Ollama
 from .constants import OLLAMA_CODE_MODEL
 
 class MermaidCodeWriter(Task):
-    def __init__(self):
-        super().__init__(
-            task_name="mermaid-code-writer",
-            llm_model=Ollama(
-                model=OLLAMA_CODE_MODEL
-            ),
-            instruction_template="""
+    def __init__(self, **kwargs):
+        kwargs["task_name"] = kwargs.get("task_name", "mermaid-code-writer")
+        kwargs["llm_model"] = kwargs.get("llm_model", Ollama(model=OLLAMA_CODE_MODEL))
+        kwargs["instruction_template"] = kwargs.get("instruction_template", """
             You are an intelligent mermaid markdown assistant that can help user create diagram basing on a given context.
             {context}
             {histories}
@@ -18,5 +15,5 @@ class MermaidCodeWriter(Task):
             Provide the markdown in mermaid language to create a diagram that best answers the user question.
             TRY to be as simple in your code as possible.
             Return markdown wrapped in ```mermaid ``` block, no explanation needed
-            """
-        )
+            """)
+        super().__init__(**kwargs)
