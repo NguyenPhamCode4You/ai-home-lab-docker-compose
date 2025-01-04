8ee550ddb3110e3b5215a76168c18a4f3b5a0716
Nguyen Pham <nguyen.pham@code4you.com>
2025-01-04 18:29:33 +0100

    update prompt

--- agentic/shipment-specialist.py
+++ agentic/shipment-specialist.py
@@ -7,6 +7,7 @@ from src.agents.MathplotCodeWriter import MathplotCodeWriter
 from src.agents.JSONSummarizer import JSONSummarizer
 from src.agents.models.ChatGpt import ChatGpt
 from src.ChatBackend import create_chat_backend
+from src.agents.models.Gemini import Gemini
 
 from dotenv import load_dotenv
 import os
@@ -21,22 +22,23 @@ api_assistant = ApiCallerAssistant(
     allowed_fields=[
         "result", "items", "vesselName", "estimateCode",
         "profitAndLost",  "freightValue", "totalProfitValue", "miscRevenuesValue", "bunkerExpenseValue", "canalTollsValue", "cargoExpenseValue", "emissionExpenseValue", "externalCommissionValue", "internalCommissionValue", "miscExpenseValue", "portExpenseValue", "vesselHireValue",
-        "iteneraryItems", "portName", "totalTimeAtPortInDays", "cargoQuantity", "shipmentName", "countryCode", "timeOfArrival", "timeOfDeparture", "reasonForVisit"
+        "iteneraryItems", "portName", "totalTimeAtPortInDays", "cargoQuantity", "shipmentName", "timeOfArrival", "timeOfDeparture", "reasonForVisit"
     ],
     llm_json_summarizer=JSONSummarizer(
         max_context_tokens=15000,
         user_instruction="""
-        Nonetheless, provide insights about the following:
-        - General information: Vessel Name, Estimate Code, etc.
-        - A table to display the itinerary items with their respective:
+        Nonetheless, do the following:
+        1. Start by providing a general overview of the estimate includes:
+        - Vessel Name, Estimate Code
+        - Total freight (freightValue) and Total profit (totalProfitValue)
+        2. Display the itenerary items in a table format:
             + Column Name = Port names (1st column) - Content: Port Name - Country Code
-            + Column Name = Arrival Time (2nd column), Content: `YYYY-MM-DD` at `HH:MM`
-            + Column Name = Departure Time (3rd column), Content: `YYYY-MM-DD` at `HH:MM`
+            + Column Name = Arrival Time (2nd column), Content: DD-MM-YYYY
+            + Column Name = Departure Time (3rd column), Content: DD-MM-YYYY
             + Column Name = Port Days (4th column), Content: Total time at port in days
             + Column Name = Shipment name - Cargo Quantity (5th column), Content: Shipment Name - Cargo Quantity
-            + Column Name = Reason for visit (6th column), Content: Reason for visit
-        - A comprehensive breakdown of expense in the profit and loss object (using bullet points)
-        - A Summary on the overall revenue: Focus on freightValue (Total income) and totalProfitValue (Total profit)
+            + Column Name = Reason (6th column), Content: Reason for visit
+        3. Provide a comprehensive breakdown of expense in the profit and loss object (using bullet points)
         """,
     ),
 )
@@ -44,15 +46,15 @@ diagram_assistant = DiagramAssistant(
     llm_mermaid_code_writter=MermaidCodeWriter(
         max_context_tokens=15000,
         user_instruction="""
-        Example template of a good timeline diagram: (Remove itenerary with reason for visit = "Commencing")
+        1. If user ask for timeline diagram, follow the below template:
         ```mermaid
         gantt
             title [Vessel Name] Timeline
-            dateFormat  YYYY-MM-DD
-            section [Port Name] - [Country Code]
-            Arrival  :[Time of arrival], 0d
-            [Shipment name - Reason for Visit]  :[Time of arrival], [Total time at port in days]d
-            Departure  :[Time of departure], 0d
+            dateFormat  DD-MM-YYYY
+            section [Port Name]
+            Arrival  :[Time of arrival format], 0d
+            [Shipment name, Reason for Visit]  :[Time of arrival format], [Total time at port in days]d
+            Departure  :[Time of departure format], 0d
         ```
         """,
     ),
@@ -60,15 +62,25 @@ diagram_assistant = DiagramAssistant(
 chart_assistant = ChartAssistant(
     show_code_stream=False,
     llm_mathplot_code_writer=MathplotCodeWriter(
-        llm_model=ChatGpt(),
+        llm_model=Gemini(),
         max_context_tokens=15000,
         user_instruction="""
-        Make 2 pie charts that:
-        1. Expenses Breakdown 
-        - Break down all the expenses structure of the estimate.
-        2. Profit Margin 
-        - Display the percentage of "Total Profit Value" over the "Freight Value". 
-        - If "Total Profit Value" is positive, use Green color, otherwise use Red color.
+        1. If user ask for cost breakdown analysis, make 2 pie charts in the same figure:
+        - Pie Chart 1: Expenses Breakdown:
+            + Break down all the expenses structure of the estimate.
+            + Use "#FF5722" color for "Bunker Expense Value".
+            + Use "#AB47BC" color for "Canal Tolls Value".
+            + Use "#BCAAA4" color for "Emission Expense Value".
+            + Use "#9E9E9E" color for "External Commission Value".
+            + Use "#757575" color for "Internal Commission Value".
+            + Use "#CDDC39" color for "Misc Expense Value".
+            + Use "#FFC107" color for "Cargo Expense Value".
+            + Use "#FF9800" color for "Port Expense Value".
+            + Use "#607D8B" color for "Vessel Hire Value".
+        - Pie Chart 2: Profit Margin chart
+            + Display the percentage of "Total Profit Value" over the "Freight Value".
+            + Use "#2196F3" color for "Freight Value".
+            + Use "#4CAF50" color for "Total Profit Value" if it is positive, otherwise use "#EF5350" color.
         """
     ),
 )
