1452c7ef42148807f96b029051e347ac130f89a7
Nicheal <nichealpham@gmail.com>
2025-01-28 21:38:26 +0100

    add ctx_num for Ollama

--- agentic/agent-orchestra-chat.py
+++ agentic/agent-orchestra-chat.py
@@ -8,7 +8,7 @@ from src.DiagramAssistant import DiagramAssistant
 from src.agents.GeneralRagAnswer import GeneralRagAnswer
 from src.agents.MermaidCodeWriter import MermaidCodeWriter
 from src.agents.models.Ollama import Ollama
-from src.agents.models.ChatGpt import ChatGpt
+# from src.agents.models.Ollama import Ollama
 from src.agents.constants import OLLAMA_CODE_MODEL
 from src.agents.DocumentRanking import DocumentRanking
 from src.agents.MathplotCodeWriter import MathplotCodeWriter
@@ -24,10 +24,10 @@ load_dotenv()
 api_assistant = ApiCallerAssistant(
     base_url="https://bvms-master-api-test.azurewebsites.net",
     llm_api_config_writter=ApiConfigWritter(
-        llm_model=ChatGpt(),
+        llm_model=Ollama(),
     ),
     llm_json_summarizer=JSONSummarizer(
-        llm_model=ChatGpt(),
+        llm_model=Ollama(),
         max_context_tokens=15000,
     ),
     bearer_token=os.getenv("BVMS_API_TOKEN"),
@@ -40,10 +40,10 @@ api_assistant = ApiCallerAssistant(
 shipment_assistant = ApiCallerAssistant(
     base_url="https://bvms-voyage-api-test.azurewebsites.net",
     llm_api_config_writter=ApiConfigWritter(
-        llm_model=ChatGpt(),
+        llm_model=Ollama(),
     ),
     llm_json_summarizer=JSONSummarizer(
-        llm_model=ChatGpt(),
+        llm_model=Ollama(),
         max_context_tokens=15000,
     ),
     bearer_token=os.getenv("BVMS_API_TOKEN"),
@@ -55,13 +55,13 @@ shipment_assistant = ApiCallerAssistant(
 
 chart_assistant = ChartAssistant(
     llm_mathplot_code_writer=MathplotCodeWriter(
-        llm_model=ChatGpt(),
+        llm_model=Ollama(),
         max_context_tokens=10000,
     )
 )
 diagram_assistant = DiagramAssistant(
     llm_mermaid_code_writter=MermaidCodeWriter(
-        llm_model=ChatGpt(),
+        llm_model=Ollama(),
         max_context_tokens=10000,
     )
 )
@@ -73,10 +73,14 @@ bvms_rag_assistant = RagAssistant(
     query_function_name="match_n8n_documents_bvms_neo",
     llm_document_ranking=DocumentRanking(
         llm_model=Ollama(),
+        # llm_model=Ollama(model="deepseek-coder-v2:16b-lite-base-q6_K"),
     ),
     llm_rag_answer=GeneralRagAnswer(
+        # max_context_tokens=12000,
+        # llm_model=Ollama(num_ctx=15200),
         max_context_tokens=6000,
-        llm_model=Ollama(),
+        llm_model=Ollama(num_ctx=8096),
+        # llm_model=Ollama(model="llama3.2:3b-instruct-fp16"),
         instruction_template="""
         You are an intelligent assistant that can provide detailed answers about a software named BVMS (BBC Voyager Management System).
         First, analyze carefully the below knowledge base to base your answer on.
@@ -89,10 +93,10 @@ bvms_rag_assistant = RagAssistant(
 be_code_assistant = RagAssistant(
     query_function_name="match_n8n_documents_net_micro_neo",
     llm_document_ranking=DocumentRanking(
-        llm_model=ChatGpt(model="o1-mini"),
+        llm_model=Ollama(model="o1-mini"),
     ),
     llm_rag_answer=GeneralRagAnswer(
-        llm_model=ChatGpt(model="o1-mini"),
+        llm_model=Ollama(model="o1-mini"),
         max_context_tokens=12000,
         instruction_template="""
         You are an intelligent coding assistants that can provide code explanations and code writing.
@@ -103,7 +107,7 @@ be_code_assistant = RagAssistant(
         """
     ))
 
-from EstimateSpecialist import estimate_specialist
+# from EstimateSpecialist import estimate_specialist
 
 assistant = AssistantOrchestra()
 assistant.agents = {
@@ -115,7 +119,7 @@ assistant.agents = {
     "Research Assistant": {"agent": research_assistant, "context_awareness": False, "description": "This agent can generate detailed web-research on complex topics"},
     "RAG Assistant": {"agent": bvms_rag_assistant, "context_awareness": False, "description": "This agent can generate detailed responses about a software named BVMS"},
     "Code Assistant": {"agent": be_code_assistant, "context_awareness": False, "description": "This agent can provide code explanations and code writing about BVMS software"},
-    "Estimate Specialist": {"agent": estimate_specialist, "context_awareness": False, "description": "This agent can provide detailed analysis of BVMS Estimates"}
+    # "Estimate Specialist": {"agent": estimate_specialist, "context_awareness": False, "description": "This agent can provide detailed analysis of BVMS Estimates"}
 }
 
 app = create_chat_backend(assistant)    
--- agentic/src/agents/models/Ollama.py
+++ agentic/src/agents/models/Ollama.py
@@ -6,15 +6,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Ollama:
-    def __init__(self, url: str = None, model: str = None):
+    def __init__(self, url: str = None, model: str = None, num_ctx: int = None):
         self.url = url or os.getenv("OLLAMA_URL") or None
         self.model = model or os.getenv("OLLAMA_GENERAL_MODEL") or None
+        self.num_ctx = num_ctx or 2048
 
     async def stream(self, prompt: str):
         if not self.url or not self.model:
             raise ValueError("URL and model must be set before using the assistant.")
         async with httpx.AsyncClient(timeout=httpx.Timeout(80.0)) as client:
-            async with client.stream("POST", f"{self.url}/api/generate", json={"model": self.model, "prompt": prompt}) as response:
+            async with client.stream("POST", f"{self.url}/api/generate", json={"model": self.model, "prompt": prompt, "options": {"num_ctx": self.num_ctx}}) as response:
                 async for chunk in response.aiter_bytes():
                     if (len(chunk) > 1000):
                         continue
