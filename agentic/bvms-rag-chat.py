07facbfa5d403d515c810272e460be8922039d83
Nguyen Pham <nguyen.pham@gmail.com>
2025-07-12 13:59:25 +0200

    Tét chat agent

--- agentic/bvms-rag-chat.py
+++ agentic/bvms-rag-chat.py
@@ -1,11 +1,17 @@
+from src.agents.models import Ollama
+from src.agents import GeneralRagAnswer
 from src.RagAssistant import RagAssistant
 from src.ChatBackend import create_chat_backend
 
 bvms_rag_assistant = RagAssistant(
     query_function_name="match_n8n_documents_bvms_neo",
-    max_context_tokens=5500)
+    llm_rag_answer=GeneralRagAnswer(
+        max_context_tokens=12000,
+        llm_model=Ollama(num_ctx=15200)
+    )
+)
 
 app = create_chat_backend(bvms_rag_assistant)    
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, timeout_keep_alive=300)
+    uvicorn.run(app, host="0.0.0.0", port=8001, timeout_keep_alive=300)
