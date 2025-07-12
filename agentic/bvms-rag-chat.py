911971c18e40559fe71b17082a01c9f1adc1fbd0
Nguyen Pham <nguyen.pham@gmail.com>
2025-07-12 19:43:14 +0200

    docker compose bvms chat

--- agentic/bvms-rag-chat.py
+++ agentic/bvms-rag-chat.py
@@ -1,15 +1,25 @@
-from src.agents.models import Ollama
-from src.agents import GeneralRagAnswer
+from src.agents.models.Ollama import Ollama
+from src.agents.GeneralRagAnswer import GeneralRagAnswer
 from src.RagAssistant import RagAssistant
 from src.ChatBackend import create_chat_backend
+from src.agents.DocumentRanking import DocumentRanking
 
 bvms_rag_assistant = RagAssistant(
-    query_function_name="match_n8n_documents_bvms_neo",
+    query_function_name="match_n8n_documents_net_micro_neo",
+    llm_document_ranking=DocumentRanking(
+        llm_model=Ollama(),
+    ),
     llm_rag_answer=GeneralRagAnswer(
-        max_context_tokens=12000,
-        llm_model=Ollama(num_ctx=15200)
-    )
-)
+        llm_model=Ollama(num_ctx=28000),
+        max_context_tokens=32000,
+        instruction_template="""
+        You are an intelligent coding assistants that can provide code explanations and code writing.
+        First, analyze carefully the code below to base your answer on.
+        {context}
+        Here is the user question: {question}
+        Try your best to assist the user with their coding question.
+        """
+    ))
 
 app = create_chat_backend(bvms_rag_assistant)    
 if __name__ == "__main__":
