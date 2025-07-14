00dc54a16d2749b780cdb687e241dc25c046a53f
Nguyen Pham <nguyen.pham@gmail.com>
2025-07-14 18:07:17 +0200

    Prepare to run imos

--- ai-c4y/rag_learn.py
+++ ai-c4y/rag_learn.py
@@ -3,19 +3,23 @@ from src.agents.MarkdownContextCleaner import MarkdownContextCleaner
 
 async def clean():
     await clean_src_folder(
-        src_folder_path='bvms',
-        target_folder_path='bvms-cleaned', 
+        src_folder_path='imos',
+        target_folder_path='imos-cleaned', 
         llm_context_cleaner=MarkdownContextCleaner(),
         context_chunk_size=600)
     print("Clean done")
 
 async def insert():
     await insert_sentences(
-        src_folder_path='bvms-cleaned',
-        table_name="n8n_documents_bvms_neo",
+        src_folder_path='imos-cleaned',
+        table_name="n8n_documents_imos_neo",
         summary_max_char=600, keyword_count=20)
     print("Insert done")
 
+async def main():
+    await clean()
+    await insert()
+
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(clean())
+    asyncio.run(main())
