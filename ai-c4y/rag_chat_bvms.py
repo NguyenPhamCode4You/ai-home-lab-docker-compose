9b3bd678109fe3f7fdbadc96620ecdbb948bf518
Nguyen Pham <nguyen.pham@gmail.com>
2026-05-17 11:59:21 +0200

    test new config

--- ai-c4y/rag_chat_bvms.py
+++ ai-c4y/rag_chat_bvms.py
@@ -9,7 +9,7 @@ from src.agents.models.OpenRouter import OpenRouter
 load_dotenv()
 
 # default_model = Ollama()
-default_model = OpenRouter(model='deepseek/deepseek-v4-flash:free')
+default_model = OpenRouter(model='qwen/qwen3.6-35b-a3b')
 
 bvms_rag_assistant = RagAssistant(
     query_function_name="match_n8n_documents_bvms_neo",
