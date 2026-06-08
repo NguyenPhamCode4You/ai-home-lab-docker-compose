5abccf5549bc225aff67af5ff7a55f0387d41b61
Nguyen Pham <nguyen.pham@code4you.com>
2026-06-08 10:46:12 +0700

    Ollama no think

--- ai-c4y/src/agents/models/Ollama.py
+++ ai-c4y/src/agents/models/Ollama.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 import httpx
 
 from dotenv import load_dotenv
@@ -8,17 +9,25 @@ load_dotenv()
 default_token_length = int(os.getenv("TOKENS_LENGTH", 28000))
 
 class Ollama:
-    def __init__(self, url: str = None, model: str = None, num_ctx: int = None, num_predict: int = None):
+    def __init__(self, url: str = None, model: str = None, num_ctx: int = None, num_predict: int = None, think: bool = False):
         self.url = url or os.getenv("OLLAMA_URL") or None
         self.model = model or os.getenv("OLLAMA_GENERAL_MODEL") or None
         self.num_ctx = num_ctx or default_token_length
         self.num_predict = num_predict or -1  # -1 = unlimited output length
+        self.think = think
 
     async def stream(self, prompt: str):
         if not self.url or not self.model:
             raise ValueError("URL and model must be set before using the assistant.")
         async with httpx.AsyncClient(timeout=httpx.Timeout(80.0)) as client:
-            async with client.stream("POST", f"{self.url}/api/generate", json={"model": self.model, "prompt": prompt, "options": {"num_ctx": self.num_ctx, "num_predict": self.num_predict}}) as response:
+            async with client.stream("POST", f"{self.url}/api/generate", json={
+                "model": self.model, 
+                "prompt": prompt, 
+                "think": self.think, 
+                "options": {
+                    "num_ctx": self.num_ctx, 
+                    "num_predict": self.num_predict
+                }}) as response:
                 async for line in response.aiter_lines():
                     line = line.strip()
                     if not line:
