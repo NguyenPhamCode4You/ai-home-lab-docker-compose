ac4f30d8adcb0706ec7b67aa950499c0c955d02e
Nguyen Pham <nguyen.pham@code4you.com>
2026-06-26 18:52:30 +0200

    customize ai-c4y docker for running

--- ai-c4y/rag_orchestra.py
+++ ai-c4y/rag_orchestra.py
@@ -25,7 +25,7 @@ assistant = AssistantOrchestra(
         llm_model=default_model,
     ),
     llm_final_thought_summarizer=FinalThoughtSummarizer(
-        llm_model=OpenRouter(model='google/gemma-4-26b-a4b-it'),
+        llm_model=default_model,
     ),
     llm_answer_evaluator=AnswerEvaluator(
         llm_model=default_model,
